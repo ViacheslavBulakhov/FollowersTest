bafac62321b0047ba82ea876a83e032fb9159ced
ViacheslavBulakhov <95303344+ViacheslavBulakhov@users.noreply.github.com>
2023-05-19 23:44:03 +0300

    Updates

---
+++ static/js/680.9e3a388b.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkfollowers_test=self.webpackChunkfollowers_test||[]).push([[680],{680:function(t,e,s){s.r(e),s.d(e,{default:function(){return r}});var i=s(184);function r(){return(0,i.jsx)("div",{style:{minHeight:"100%",borderRadius:"20px",overflow:"hidden"},children:(0,i.jsx)("img",{src:"https://picsum.photos/1920/1080",alt:"",style:{width:"100%",height:"100%",objectFit:"cover"}})})}}}]);
+//# sourceMappingURL=680.9e3a388b.chunk.js.map
