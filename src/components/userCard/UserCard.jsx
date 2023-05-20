7a7a3ba48df75aee867db449042232e8f45933c7
ViacheslavBulakhov <95303344+ViacheslavBulakhov@users.noreply.github.com>
2023-05-20 13:29:05 +0300

    fix image user

--- src/components/userCard/UserCard.jsx
+++ src/components/userCard/UserCard.jsx
@@ -87,7 +87,7 @@ export default function UserCard({
         />
       </UserPhotoWrap>
       <DelimiterCardElement />
-      <ImageFrameElement />
+      {avatar && <ImageFrameElement />}
 
       <ul
         style={{
